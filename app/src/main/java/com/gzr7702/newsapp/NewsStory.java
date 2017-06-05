231d576083f3c307114e71c0cc579212939b336f
gzr7702 <rgraziano.gzr7702@gmail.com>
2017-06-05 19:15:47 -0400

    feat: added NewsStory object

--- app/src/main/java/com/gzr7702/newsapp/NewsStory.java
+++ app/src/main/java/com/gzr7702/newsapp/NewsStory.java
@@ -5,4 +5,39 @@ package com.gzr7702.newsapp;
  */
 
 public class NewsStory {
+    private String title;
+    private String author;
+    private String section;
+    private String summary;
+    private String publishDate;
+
+    public NewsStory (String title, String author, String section, String summary,
+                 String publishDate) {
+        super();
+        this.title = title;
+        this.author = author;
+        this.section = section;
+        this.summary = summary;
+        this.publishDate = publishDate;
+    }
+
+    public String getTitle() {
+        return this.title;
+    }
+
+    public String getAuthor() {
+        return this.author;
+    }
+
+    public String getSection() {
+        return this.section;
+    }
+
+    public String getSummary () {
+        return this.summary;
+    }
+
+    public String getPublishDate() {
+        return this.publishDate;
+    }
 }
