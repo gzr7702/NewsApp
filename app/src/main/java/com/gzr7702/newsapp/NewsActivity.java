f518ecce249bfee47ee101a88f5d10558c23101d
gzr7702 <rgraziano.gzr7702@gmail.com>
2017-06-09 23:22:21 -0400

    fix: tweaked front-end

--- app/src/main/java/com/gzr7702/newsapp/NewsActivity.java
+++ app/src/main/java/com/gzr7702/newsapp/NewsActivity.java
@@ -11,7 +11,6 @@ import android.net.Uri;
 import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
